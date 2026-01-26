4d99373c7bcd816242d81f72ae5130e858278be0
Miclasher <14122006mg@gmail.com>
2026-01-26 20:17:55 +0200

    Map static assets

--- FinanceManagmentApp.Frontend/Program.cs
+++ FinanceManagmentApp.Frontend/Program.cs
@@ -42,7 +42,7 @@ public static class Program
 
         app.UseHttpsRedirection();
 
-        app.UseStaticFiles();
+        app.MapStaticAssets();
         app.UseAntiforgery();
 
         app.MapRazorComponents<App>()
